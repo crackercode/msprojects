436209e1217d590fe8f0bcdfb2cc0054d33c7877
cracker <crackerit92@gmail.com>
2022-09-08 14:17:48 +0530

    Add AZ

---
+++ azserice/src/main/java/com/prac/azserice/AzsericeApplication.java
@@ -0,0 +1,13 @@
+package com.prac.azserice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzsericeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzsericeApplication.class, args);
+	}
+
+}
